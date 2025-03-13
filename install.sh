803196721f534b337a34ee08b2217eeb82bc0197
SHARE IT HUB <156384686+shareithub@users.noreply.github.com>
2025-03-13 22:10:18 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 clear
 # Define colors
 RED='\033[0;31m'
@@ -34,73 +33,19 @@ echo -e "${MAGENTA} │${NC} ${CYAN}Provider      : DigitalOcean${NC}           
 echo -e "${MAGENTA} │${NC} ${CYAN}Expiry In     : ${remaining_days} days remaining${NC}               ${MAGENTA}│ ${NC}"
 echo -e "${MAGENTA} └────────────────────────────────────────────────────────────────────────────────────────┘ ${NC}"
 
-echo -e "${RED} ┌────────────────────────────────────────────────┐ ${NC}"
-echo -e "${RED} │${NC}         ${CYAN}Select Your Windows Version${NC}           ${RED}│ ${NC}"
-echo -e "${RED} ├────────────────────────────────────────────────┤ ${NC}"
-echo -e "${RED} │${NC} ${CYAN}1) Windows Server 2012${NC}                             ${RED}│ ${NC}"
-echo -e "${RED} │${NC} ${CYAN}2) Windows Server 2016${NC}                             ${RED}│ ${NC}"
-echo -e "${RED} │${NC} ${CYAN}3) Windows Server 2019${NC}                             ${RED}│ ${NC}"
-echo -e "${RED} │${NC} ${CYAN}4) Windows Server 2022${NC}                             ${RED}│ ${NC}"
-echo -e "${RED} │${NC} ${CYAN}5) Windows Server 10${NC}                              ${RED}│ ${NC}"
-echo -e "${RED} │${NC} ${CYAN}6) Windows Server 11${NC}                              ${RED}│ ${NC}"
-echo -e "${RED} └────────────────────────────────────────────────┘ ${NC}"
+# Langsung memilih Windows Server 2022
+PILIHOS="https://sourceforge.net/projects/shareithub/files/shareithub/windows2022.gz"
+echo -e "${CYAN}Memulai instalasi Windows 2022...${NC}"
 
-echo -n "Pilih (1-6): "
-read pilihan
+# Menggunakan kata sandi default untuk akun Administrator
+PASSWORD="Nixpoin.com123!"
+echo -e "${CYAN}Menggunakan kata sandi default: $PASSWORD${NC}"
 
-if [[ -z "$pilihan" ]]; then
-    echo -e "${RED}Tidak ada input yang diterima. Pastikan console mendukung input interaktif.${NC}"
-    exit 1
-fi
-
-case $pilihan in
-    1)
-        PILIHOS="https://sourceforge.net/projects/nixpoin/files/windows2012.gz"
-        echo -e "${CYAN}Memulai instalasi Windows 2012...${NC}"
-        ;;
-    2)
-        PILIHOS="https://sourceforge.net/projects/nixpoin/files/windows2016.gz"
-        echo -e "${CYAN}Memulai instalasi Windows 2016...${NC}"
-        ;;
-    3)
-        PILIHOS="https://sourceforge.net/projects/nixpoin/files/windows2019.gz"
-        echo -e "${CYAN}Memulai instalasi Windows 2019...${NC}"
-        ;;
-    4)
-        PILIHOS="https://sourceforge.net/projects/shareithub/files/shareithub/windows2022.gz"
-        echo -e "${CYAN}Memulai instalasi Windows 2022...${NC}"
-        ;;
-    5)
-        PILIHOS="https://sourceforge.net/projects/nixpoin/files/windows10.gz"
-        echo -e "${CYAN}Memulai instalasi Windows 10...${NC}"
-        ;;
-    6)
-        PILIHOS="https://sourceforge.net/projects/nixpoin/files/windows11.gz"
-        echo -e "${CYAN}Memulai instalasi Windows 11...${NC}"
-        ;;
-    *)
-        echo -e "${RED}Pilihan tidak valid!${NC}"
-        exit 1
-        ;;
-esac
-
-echo -e "${CYAN}Apakah Anda ingin mengatur kata sandi untuk akun Administrator?${NC}"
-echo -e "${CYAN}1) Ya, saya ingin mengatur kata sandi saya sendiri${NC}"
-echo -e "${CYAN}2) Tidak, gunakan kata sandi default${NC}"
-echo -n "Pilih (1-2): "
-read set_password
-
-if [ "$set_password" -eq 1 ]; then
-    read -sp "Masukkan kata sandi untuk akun Administrator: " PASSWORD
-    echo
-else
-    PASSWORD="Nixpoin.com123!"
-    echo -e "${CYAN}Menggunakan kata sandi default: $PASSWORD${NC}"
-fi
-
+# Mengambil IP publik dan gateway
 IP4=$(curl -4 -s icanhazip.com)
 GW=$(ip route | awk '/default/ { print $3 }')
 
+# Membuat file net.bat untuk pengaturan IP dan DNS secara otomatis
 cat >/tmp/net.bat<<EOF
 @ECHO OFF
 cd.>%windir%\GetAdmin
@@ -120,15 +65,18 @@ del /f /q net.bat
 exit
 EOF
 
+# Mengunduh dan menginstall image OS Windows 2022
 echo "Mendownload file $PILIHOS..."
 wget --no-check-certificate -O- $PILIHOS | gunzip | dd of=/dev/vda bs=3M status=progress
 
+# Mount partisi dan menyiapkan file setup
 mount.ntfs-3g /dev/vda2 /mnt
 cd "/mnt/ProgramData/Microsoft/Windows/Start Menu/Programs/"
 cd Start* || cd start*; \
 wget https://nixpoin.com/ChromeSetup.exe
 cp -f /tmp/net.bat net.bat
 
+# Shutdown
 echo -e "${RED}Your server will turn off in 3 seconds...${NC}"
 sleep 3
 poweroff
